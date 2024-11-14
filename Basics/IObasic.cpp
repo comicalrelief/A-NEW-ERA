4078b3a9071b5eafb05a49ea967a067149cc4406
comicalrelief <balapranav.k@gmail.com>
2024-11-14 18:20:32 +0530

    Create IObasic.cpp

---
+++ Basics/IObasic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number: " << endl; 
+    cin >> a;
+    cout << "The number you entered is: " << a;
+
+    return 0;
+}
